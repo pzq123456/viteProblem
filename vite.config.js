b59ee9cddcbf4530a97b8c8fa4fc3f2963180f76
潘志清 <82391775+pzq123456@users.noreply.github.com>
2023-05-05 22:47:17 +0800

    done!

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,21 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@cornerstonejs/dicom-image-loader': '@cornerstonejs/dicom-image-loader/dist/dynamic-import/cornerstoneDICOMImageLoader.min.js',
     }
-  }
+  },
+  // config settings for dev server
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+
+    //
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  },
 })
